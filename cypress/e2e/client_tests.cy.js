d9bf665c5d70f74b99a8a30a3697cc2cd8c4e0a5
Tommi Koivula <tommi.koivula93@gmail.com>
2023-06-28 14:49:22 +0300

    update e2e tests

--- cypress/e2e/client_tests.cy.js
+++ cypress/e2e/client_tests.cy.js
@@ -3,4 +3,25 @@ describe('Message app', () => {
     cy.visit('http://localhost:8000')
     cy.contains('Welcome')
   })
+  it('adding a new message', () => {
+    cy.visit('http://localhost:8000')
+    cy.contains('Messages').click()
+    cy.contains('Messages')
+    const msg = 'New message'
+    cy.get('#message').type(msg)
+    cy.contains('Send!').click()
+    cy.contains(msg)
+    cy.contains('delete').click()
+  })
+  it('deleting a message', () => {
+    cy.visit('http://localhost:8000/messages')
+    const msg = 'to be deleted'
+    cy.get('#message').type(msg)
+    cy.contains('Send!').click()
+    cy.contains(msg)
+    cy.contains('delete').click()
+    cy.get('ul').within(() => {
+      cy.get('li').should('not.have', msg)
+    })
+  })
 })
