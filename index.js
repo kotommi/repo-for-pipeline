b6c7cc60ec4e0b1dd0a032ba6feb520fc2b0d5e0
Tommi Koivula <tommi.koivula93@gmail.com>
2023-06-29 15:16:13 +0300

    testing error case

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 
 // Require is here so we can delete it from cache when files change (*)
 app.use('/api', (req, res, next) => require('@root/server')(req, res, next)) // eslint-disable-line
-
+console.log("asd")
 /**
  *  Use "hot loading" in backend
  */
